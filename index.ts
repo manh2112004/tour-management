883af44a75b2a637fd9b7e9cc1746f8b14281759
Mạnh <creshtthuyquai@gmail.com>
2025-09-29 21:44:22 +0700

    cau hinh chung

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+const port: number = 3000;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Trang chủ");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
