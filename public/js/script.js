94cb44e1a5f2ba9d32f8bc060bd8903e146de08a
Mạnh <creshtthuyquai@gmail.com>
2025-10-01 13:59:32 +0700

    them tour vao gio hang

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,53 @@ var imagesMain = new Swiper(".imagesMain", {
   },
 });
 // End Slider Tour Detail
+const alertAddCartSuccess = () => {
+  const elementAlert = document.querySelector("[alert-add-cart-success");
+  if (elementAlert) {
+    elementAlert.classList.remove("alert-hidden");
+    setTimeout(() => {
+      elementAlert.classList.add("alert-hidden");
+    }, 3000);
+    const closeAlert = elementAlert.querySelector("[close-alert]");
+    closeAlert.addEventListener("click", () => {
+      elementAlert.classList.add("alert-hidden");
+    });
+  }
+};
+// carts
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+const showMiniCart = () => {
+  const miniCart = document.querySelector("[mini-cart]");
+  if (miniCart) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    miniCart.innerHTML = cart.length;
+  }
+};
+showMiniCart();
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      const indexExitsTour = cart.findIndex((item) => item.tourId === tourId);
+      if (indexExitsTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity,
+        });
+      } else {
+        cart[indexExitsTour].quantity += quantity;
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+      alertAddCartSuccess();
+      showMiniCart();
+    }
+  });
+}
+// end carts
